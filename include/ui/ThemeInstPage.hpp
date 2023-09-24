efe355b29a38635fd8459241cd34a81dd646aa02
mrdude2478 <mrdude2478@gmail.com>
2023-09-24 02:37:27 +0100

    Add files via upload

---
+++ include/ui/ThemeInstPage.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <pu/Plutonium>
+
+using namespace pu::ui::elm;
+namespace inst::ui {
+	class ThemeInstPage : public pu::ui::Layout
+	{
+	public:
+		ThemeInstPage();
+		PU_SMART_CTOR(ThemeInstPage)
+		void startNetwork();
+		void onInput(u64 Down, u64 Up, u64 Held, pu::ui::TouchPoint touch_pos);
+		TextBlock::Ref pageInfoText;
+	private:
+		std::vector<std::string> ourUrls;
+		std::vector<std::string> modded;
+		std::vector<std::string> selectedUrls;
+		std::vector<std::string> alternativeNames;
+		TextBlock::Ref butText;
+		Rectangle::Ref topRect;
+		Rectangle::Ref infoRect;
+		Rectangle::Ref botRect;
+		Image::Ref titleImage;
+		TextBlock::Ref appVersionText;
+		pu::ui::elm::Menu::Ref menu;
+		Image::Ref infoImage;
+		void drawMenuItems(bool clearItems);
+		void drawMenuItems_withext(bool clearItems);
+		void selectTitle(int selectedIndex);
+	};
+}
