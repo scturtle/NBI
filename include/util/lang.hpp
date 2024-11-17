5fd18c582c6c5bffc8eab80957c790b08d779c43
scturtle <scturtle@gmail.com>
2024-11-18 00:48:32 +0800

    lang

--- include/util/lang.hpp
+++ include/util/lang.hpp
@@ -1,26 +1,13 @@
 #pragma once
 
-#include "nlohmann/json.hpp"
 #include <fstream>
 #include <sstream>
 #include <string>
-
-using json = nlohmann::json;
 
 namespace Language {
 void Load();
 std::string LanguageEntry(std::string key);
 std::string GetRandomMsg();
-inline json GetRelativeJson(json j, std::string key) {
-  std::istringstream ss(key);
-  std::string token;
-
-  while (std::getline(ss, token, '.') && j != nullptr) {
-    j = j[token];
-  }
-
-  return j;
-}
 } // namespace Language
 
 inline std::string operator""_lang(const char *key, size_t size) {
--- source/util/lang.cpp
+++ source/util/lang.cpp
@@ -1,10 +1,13 @@
 #include "util/lang.hpp"
+#include "nlohmann/json.hpp"
 #include "util/config.hpp"
 #include <cstring>
 #include <filesystem>
 #include <iostream>
 #include <sstream>
 #include <switch.h>
+
+using json = nlohmann::json;
 
 namespace Language {
 json lang;
@@ -19,106 +22,50 @@ void Load() {
   setMakeLanguage(lcode, &ourLang);
   setExit();
   int syslang = (int)ourLang;
-  std::ifstream ifs;
   std::string languagePath;
   int langInt = inst::config::languageSetting;
   if (std::filesystem::exists(inst::config::appDir + "/lang/custom.json")) {
     languagePath = (inst::config::appDir + "/lang/custom.json");
   } else {
+    // clang-format off
     switch (langInt) {
-    case 1:
-      languagePath = "romfs:/lang/en.json";
-      break;
-    case 2:
-      languagePath = "romfs:/lang/jp.json";
-      break;
-    case 3:
-      languagePath = "romfs:/lang/fr.json";
-      break;
-    case 4:
-      languagePath = "romfs:/lang/de.json";
-      break;
-    case 5:
-      languagePath = "romfs:/lang/it.json";
-      break;
-    case 6:
-      languagePath = "romfs:/lang/ru.json";
-      break;
-    case 7:
-      languagePath = "romfs:/lang/es.json";
-      break;
-    case 8:
-      languagePath = "romfs:/lang/tw.json";
-      break;
-    case 9:
-      languagePath = "romfs:/lang/cn.json";
-      break;
-    case 0:
-      switch (syslang) {
-      case 0: // Japanese
-        languagePath = "romfs:/lang/jp.json";
+      case 1: languagePath = "romfs:/lang/en.json"; break;
+      case 2: languagePath = "romfs:/lang/jp.json"; break;
+      case 3: languagePath = "romfs:/lang/fr.json"; break;
+      case 4: languagePath = "romfs:/lang/de.json"; break;
+      case 5: languagePath = "romfs:/lang/it.json"; break;
+      case 6: languagePath = "romfs:/lang/ru.json"; break;
+      case 7: languagePath = "romfs:/lang/es.json"; break;
+      case 8: languagePath = "romfs:/lang/tw.json"; break;
+      case 9: languagePath = "romfs:/lang/cn.json"; break;
+      case 0:
+        switch (syslang) {
+          case 0: languagePath = "romfs:/lang/jp.json"; break;
+          case 1: languagePath = "romfs:/lang/en.json"; break;
+          case 2: languagePath = "romfs:/lang/fr.json"; break;
+          case 3: languagePath = "romfs:/lang/de.json"; break;
+          case 4: languagePath = "romfs:/lang/it.json"; break;
+          case 5: languagePath = "romfs:/lang/es.json"; break;
+          case 6: languagePath = "romfs:/lang/cn.json"; break;
+          case 7: languagePath = "romfs:/lang/en.json"; break;
+          case 8: languagePath = "romfs:/lang/en.json"; break;
+          case 9: languagePath = "romfs:/lang/en.json"; break;
+          case 10: languagePath = "romfs:/lang/ru.json"; break;
+          case 11: languagePath = "romfs:/lang/tw.json"; break;
+          case 12: languagePath = "romfs:/lang/en.json"; break;
+          case 13: languagePath = "romfs:/lang/fr.json"; break;
+          case 14: languagePath = "romfs:/lang/es.json"; break;
+          case 15: languagePath = "romfs:/lang/tw.json"; break;
+          case 16: languagePath = "romfs:/lang/cn.json"; break;
+          case 17: languagePath = "romfs:/lang/en.json"; break;
+          default: languagePath = "romfs:/lang/en.json"; break;
+        }
         break;
-      case 1: // AmericanEnglish
-        languagePath = "romfs:/lang/en.json";
-        break;
-      case 2: // French
-        languagePath = "romfs:/lang/fr.json";
-        break;
-      case 3: // German
-        languagePath = "romfs:/lang/de.json";
-        break;
-      case 4: // Italian
-        languagePath = "romfs:/lang/it.json";
-        break;
-      case 5: // Spanish
-        languagePath = "romfs:/lang/es.json";
-        break;
-      case 6: // Chinese
-        languagePath = "romfs:/lang/cn.json";
-        break;
-      case 7: // Korean
-        languagePath = "romfs:/lang/en.json";
-        break;
-      case 8: // Dutch
-        languagePath = "romfs:/lang/en.json";
-        break;
-      case 9: // Portuguese
-        languagePath = "romfs:/lang/en.json";
-        break;
-      case 10: // Russian
-        languagePath = "romfs:/lang/ru.json";
-        break;
-      case 11: // Taiwanese
-        languagePath = "romfs:/lang/tw.json";
-        break;
-      case 12: // BritishEnglish
-        languagePath = "romfs:/lang/en.json";
-        break;
-      case 13: // CanadianFrench
-        languagePath = "romfs:/lang/fr.json";
-        break;
-      case 14: // LatinAmericanSpanish
-        languagePath = "romfs:/lang/es.json";
-        break;
-      case 15: // SimplifiedChinese
-        languagePath = "romfs:/lang/tw.json";
-        break;
-      case 16: // TraditionalChinese
-        languagePath = "romfs:/lang/cn.json";
-        break;
-      case 17: // BrazilianPortuguese
-        languagePath = "romfs:/lang/en.json";
-        break;
-      default:
-        languagePath = "romfs:/lang/en.json";
-        break;
-      }
-      break;
-    default:
-      languagePath = "romfs:/lang/en.json";
-      break;
+      default: languagePath = "romfs:/lang/en.json"; break;
     }
+    // clang-format on
   }
+  std::ifstream ifs;
   if (std::filesystem::exists(languagePath))
     ifs = std::ifstream(languagePath);
   else
@@ -129,6 +76,15 @@ void Load() {
   }
   lang = json::parse(ifs);
   ifs.close();
+}
+
+inline json GetRelativeJson(json j, std::string key) {
+  std::istringstream ss(key);
+  std::string token;
+  while (std::getline(ss, token, '.') && j != nullptr) {
+    j = j[token];
+  }
+  return j;
 }
 
 std::string LanguageEntry(std::string key) {
